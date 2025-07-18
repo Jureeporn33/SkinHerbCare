cb9e2b54d61154d7097785afc678dfa7f03d67d6
Orawee Thapar-sa <159878420+Orawee47@users.noreply.github.com>
2025-07-18 21:18:07 +0700

    Add files via upload

---
+++ resize.py
@@ -0,0 +1,40 @@
+#รีไซส์ + แปลงชื่อไฟล์
+import os
+from PIL import Image
+import pillow_heif
+
+pillow_heif.register_heif_opener()  # 🛠 บรรทัดสำคัญ!!
+
+input_folder = '\Dataset\cucumber'
+output_folder = r'C:\Dataset\img resize\cucumber'
+resize_size = (224, 224)  # ขนาดที่ต้องการรีไซส์
+
+os.makedirs(output_folder, exist_ok=True)
+
+file_list = os.listdir(input_folder)
+print(f"ไฟล์ทั้งหมดใน {input_folder}: {file_list}")
+found = False
+supported_exts = ('.heic', '.jpg', '.jpeg', '.png')
+count = 1
+success = 0
+fail = 0
+for filename in file_list:
+    if filename.lower().endswith(supported_exts):
+        found = True
+        file_path = os.path.join(input_folder, filename)
+        try:
+            image = Image.open(file_path)
+            image = image.resize(resize_size)
+            new_filename = f"Cucumber_{count:03d}.png" # เปลี่ยนชื่อไฟล์ใหม่เป็นชื่อสมุนไพรนั้นๆ
+            save_path = os.path.join(output_folder, new_filename)
+            image.save(save_path, format="PNG")
+            print(f"✅ แปลง {filename} เป็น {new_filename} เรียบร้อยแล้ว")
+            count += 1
+            success += 1
+        except Exception as e:
+            print(f"❌ แปลง {filename} ไม่ได้: {e}")
+            fail += 1
+if not found:
+    print(f"ไม่พบไฟล์ที่รองรับ ({supported_exts}) ในโฟลเดอร์นี้!")
+print(f"\nสรุป: แปลงสำเร็จ {success} ไฟล์ | ล้มเหลว {fail} ไฟล์")
+print("🎉 เสร็จแล้วค้าบบบ!")
